4fd02cf660d7e4e34036262c1afb7479061e59e3
Love Kumar <114349412+codewithlovekumar@users.noreply.github.com>
2024-04-06 18:54:20 +0000

    Udate Day-02

---
+++ Day-02/string.js
@@ -0,0 +1,18 @@
+let msg1 = "Welcome to";
+let msg2 = 'JavaScript';
+let msg3=`Practice`;
+console.log(msg1);
+console.log(msg2);
+console.log(msg3);
+
+console.log(msg1,msg2,msg3);
+console.log(msg1+msg2+msg3);
+console.log(msg1+" "+msg2+" "+msg3);
+console.log("msg1 msg2 msg3");
+console.log(`${msg1} ${msg2} ${msg3}`);
+
+let msg4 ="hello in Javascrit practice"
+let res1 = msg4.toUpperCase();
+let res2 = msg4.toLowerCase();
+console.log(res1,res2);
+// document.getElementById('root').innerHTML=res1;
