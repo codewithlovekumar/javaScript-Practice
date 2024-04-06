828969e2297d1197b122b47ab0d439ca1104c4b1
Love Kumar <114349412+codewithlovekumar@users.noreply.github.com>
2024-04-06 18:20:58 +0000

    Update

---
+++ Day-02/dataTypes.js
@@ -0,0 +1,25 @@
+let num1 = 20;
+let num2 = 10;
+
+console.log(typeof num1);
+console.log(num1,num2);
+console.log(num1+num2);
+
+let float = 10.25;
+console.log(typeof float);
+
+let message = "Hello";
+console.log(message);
+console.log(typeof message);
+
+let a="10";
+let b="20";
+
+console.log(typeof a+b);
+
+let data3 = new Number(1000);
+console.log(data3);
+
+
+let num3 = 10.50;
+console.log(typeof num3);
