ed1a4afd7444a449c599f65e4d73f8d91ede8a17
Love Kumar <114349412+codewithlovekumar@users.noreply.github.com>
2024-04-08 17:27:39 +0000

    update

---
+++ Day-03/function.js
@@ -0,0 +1,14 @@
+// type 1
+
+const app = function hello() { return "Hello" };
+console.log(app());
+
+// type 2 un-named function
+const app1 = function () { return "hello 1" };
+console.log(app1());
+
+
+// type 3 arraow function
+const app2 = () => "hello";
+console.log(app2());
+
