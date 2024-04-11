bc66219c527e1619d41207afe535600257030796
Love Kumar <114349412+codewithlovekumar@users.noreply.github.com>
2024-04-11 23:34:58 +0530

    Create arrow-function.js

---
+++ Day-06/arrow-function.js
@@ -0,0 +1,18 @@
+let app = function () {
+    return "Hello Admin";
+}
+console.log(app());
+
+
+
+// example
+
+let Ducat = () => "Hello Boss";
+
+console.log(Ducat());
+
+// example 2
+
+let add = (a, b) => a + b;
+
+console.log(add(10, 50));
